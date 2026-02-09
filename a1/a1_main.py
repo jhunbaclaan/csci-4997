a9d729639f2b21faf8c8bb699399e19c9603f3f8
june <54416373+jhunbaclaan@users.noreply.github.com>
2026-02-08 23:45:31 -1000

    a1 done

--- a1/a1_main.py
+++ a1/a1_main.py
@@ -1,16 +1,20 @@
 ## IMPORTANT: RUN FROM ROOT DIRECTORY!!! (csci-4997/) 
 # for some god forsaken reason this just wont run if you try running from ../a1
+# this MIGHT just be a macos only issue. run it from ../csci-4997 and you'll be fine
 
 # refuses to run in vsc, so use console
-# please please PLEASE run with THIS ---> python -m a1.a1_main
+# note to self: please please PLEASE run with THIS ---> python3 -m a1.a1_main
 
 # tester file for all a1 methods
 
 import os
 import sys
+import numpy as np
 sys.path.insert(0, os.path.abspath('a1/4997-tools-v4/software/models'))
 from a1.A1Part1 import readAudio
 from a1.A1Part2 import minMaxAudio
+from a1.A1Part3 import hopSamples
 
 print("part 1:",readAudio('piano.wav'))
 print("part 2:", minMaxAudio('oboe-A4.wav'))  # test part 2
+print("part 3:", hopSamples(np.arange(10), 2))  # test part 3
